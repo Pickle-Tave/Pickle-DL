532d59601150469a00e024582dc248eca89ec105
ben8169 <ben8169@outlook.kr>
2024-07-14 19:12:51 +0900

    Update Final app.py
    Model server code complete

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from email.policy import strict
 import os
 import shutil
 import numpy as np
@@ -13,6 +14,9 @@ from ultralytics import YOLO
 import faiss
 import cv2
 import warnings
+from torchvision.models import EfficientNet_B2_Weights,MobileNet_V2_Weights 
+
+
 
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -21,31 +25,34 @@ app = FastAPI()
 
 class ImageRequest(BaseModel):
     imageUrls: List[str]
-    strongClustering: Optional[bool] = False
+    strongClustering: Optional[bool] = True
     eyeClosing: Optional[bool] = False
     blurred: Optional[bool] = False
-
-# 공통 이미지 전처리
-preprocess = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-])
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ClusteringModel:
     def __init__(self):
-        self.model = models.efficientnet_b0(pretrained=True)
+        self.model = models.efficientnet_b2(weights=EfficientNet_B2_Weights.IMAGENET1K_V1)
         self.model.classifier[1] = nn.Identity()  # Remove the classification layer
         self.model = self.model.to(device)
-        self.threshold = 0.7  # Default threshold
+        self.threshold = 0.65  
+        self.imagesize = 288
+        self.preprocess = self.get_preprocess(self.imagesize)
+
+    def get_preprocess(self, imagesize):
+        return transforms.Compose([
+            transforms.Resize(imagesize),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
     def set_threshold(self, strong_clustering):
-        self.threshold = 0.7 if strong_clustering else 0.65
+        self.imagesize = 288 if strong_clustering else 512
+        self.preprocess = self.get_preprocess(self.imagesize)
 
     def get_image_embedding(self, image):
-        img_tensor = preprocess(image).unsqueeze(0).to(device)
+        img_tensor = self.preprocess(image).unsqueeze(0).to(device)
         self.model.eval()
         with torch.no_grad():
             embedding_vector = self.model(img_tensor).cpu().numpy().flatten()
@@ -82,61 +89,82 @@ class ClusteringModel:
         
         return groups
 
+
 class BlurModel:
     def __init__(self, model_path):
-        self.model = models.mobilenet_v2(pretrained=True)
-        self.model.classifier[1] = nn.Linear(self.model.last_channel, 2)  # Assuming 2 classes: blur and sharp
-        self.model.load_state_dict(torch.load(model_path, map_location=device))
+        self.model = models.mobilenet_v2(weights=MobileNet_V2_Weights.DEFAULT)
+        self.model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+        self.model.classifier[1] = nn.Linear(self.model.last_channel, 2) 
+
+        blur_state_dict = torch.load(model_path, map_location=device)
+        new_blur_state_dict = {}
+        for k, v in blur_state_dict.items():
+            new_key = k.replace('model.', '')
+            new_blur_state_dict[new_key] = v
+        self.model.load_state_dict(new_blur_state_dict,strict=True)
         self.model.to(device)
+
+        self.preprocess = transforms.Compose([
+            transforms.Resize((224, 224)),
+            transforms.Grayscale(num_output_channels=1),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])
  
     def predict_blur(self, image):
-        image_tensor = preprocess(image).unsqueeze(0).to(device)
+        image_tensor = self.preprocess(image).unsqueeze(0).to(device)
         self.model.eval()
         with torch.no_grad():
             output = self.model(image_tensor)
             _, predicted = torch.max(output, 1)
-        print(predicted)        
-        if predicted == 0:
-            return False
-        else:
-            return True
-
+        
+        return predicted.item() == 1
 
 class EyeStateDetector:
     def __init__(self, model_path):
         self.yolo_model = YOLO("models/yolov8/yolov8n-face.pt")
         self.efficientnet_model = self.load_efficientnet_model(model_path)
-        self.class_names = ['ClosedFace', 'OpenFace']
+        self.preprocess = transforms.Compose([
+            transforms.Resize(288),
+            transforms.CenterCrop(256),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
 
     def load_efficientnet_model(self, model_path):
-        net = models.efficientnet_b0(pretrained=True)
+        net = models.efficientnet_b2(weights=EfficientNet_B2_Weights.IMAGENET1K_V1)
         in_features = net.classifier[1].in_features
         net.classifier[1] = nn.Linear(in_features, 2)
-        net.load_state_dict(torch.load(model_path, map_location=device), strict=False)
+        net.load_state_dict(torch.load(model_path, map_location=device),strict=True)
         net.to(device)
         net.eval()
         return net
 
     def face_detection(self, image):
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = self.yolo_model.predict(source=rgb_image, conf=0.6, max_det=15, verbose=False)
+        results = self.yolo_model.predict(source=rgb_image, conf=0.7, max_det=11, verbose=False)
+
+        total_bbox = sum(len(result.boxes.xyxy) for result in results)
+        if total_bbox > 7:
+            return []
+        
         faces = []
         for result in results:
             for bbox in result.boxes.xyxy:
                 x1, y1, x2, y2 = map(int, bbox)
                 face_width, face_height = x2 - x1, y2 - y1
-                if face_width >= 200 and face_height >= 200:
+                if face_width >= 45 and face_height >= 45:
                     face_image = image[y1:y2, x1:x2]
                     faces.append(face_image)
         return faces
 
     def eye_detection(self, face_image):
         image = Image.fromarray(cv2.cvtColor(face_image, cv2.COLOR_BGR2RGB))
-        image = preprocess(image).unsqueeze(0).to(device)
+        image = self.preprocess(image).unsqueeze(0).to(device)
         with torch.no_grad():
             outputs = self.efficientnet_model(image)
             _, preds = torch.max(outputs, 1)
-        return self.class_names[preds.item()]
+        return preds
 
     def predict(self, image_path):
         image = cv2.imread(image_path)
@@ -145,14 +173,14 @@ class EyeStateDetector:
             return False
         for face in faces:
             eye_state = self.eye_detection(face)
-            if eye_state == 'ClosedFace':
+            if eye_state == 0:
                 return True
         return False
 
 # FastAPI 엔드포인트
 clustering_model = ClusteringModel()
-blur_model = BlurModel("models/blurred_0620.pth")
-eye_state_detector = EyeStateDetector("models/eyeclosing_0622.pth")
+blur_model = BlurModel("models/blurred_0711.pth")
+eye_state_detector = EyeStateDetector("models/eyeclosing_0714.pth")
 
 @app.post("/images/classify")
 def process_urls(request: ImageRequest):
@@ -173,24 +201,24 @@ def process_urls(request: ImageRequest):
                 with open(image_path, 'wb') as f:
                     f.write(response.content)
                 image_paths.append(image_path)
-                # print(f"Downloaded image: {image_path}")  # 로그 추가
+                # print(f"Downloaded image: {image_path}")  
         except Exception as e:
-            print(f"Failed to download image from {url}: {e}")  # 로그 추가
+            print(f"Failed to download image from {url}: {e}")  
 
     for image_path in image_paths:
         try:
             image = Image.open(image_path).convert('RGB')
             if request.blurred and blur_model.predict_blur(image):
                 os.remove(image_path)
-                print(f"Removed blurred image: {image_path}")  # 로그 추가
+                # print(f"Removed blurred image: {image_path}")  
                 continue
 
             if request.eyeClosing and eye_state_detector.predict(image_path):
                 os.remove(image_path)
-                print(f"Removed eye-closing image: {image_path}")  # 로그 추가
+                # print(f"Removed eye-closing image: {image_path}")  
                 continue
         except Exception as e:
-            print(f"Failed to process image: {image_path}, Error: {e}")  # 로그 추가
+            print(f"Failed to process image: {image_path}, Error: {e}")  
 
     remaining_images = [image_path for image_path in image_paths if os.path.exists(image_path)]
     clustering_model.set_threshold(request.strongClustering)
@@ -200,7 +228,6 @@ def process_urls(request: ImageRequest):
         for j in range(len(grouped_images[i])):
             url_idx = int(os.path.basename(grouped_images[i][j]).rstrip(".png"))
             grouped_images[i][j] = request.imageUrls[url_idx]
-
 
     shutil.rmtree(member_folder)
 
